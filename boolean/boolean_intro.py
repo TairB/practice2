0afd088633a947a0e3ae5fce54cb34ffa7092bef
TairB <ttair47@gmail.com>
2026-02-04 04:16:44 +0500

    Add boolean examples

---
+++ boolean/boolean_intro.py
@@ -0,0 +1,26 @@
+# Boolean Intro
+# True and False basic examples
+
+print(True)
+print(False)
+
+# Using bool() with different values
+print(bool("Hello"))
+print(bool(""))
+print(bool(10))
+print(bool(0))
+
+# Variables as booleans
+is_student = True
+is_teacher = False
+
+print(is_student)
+print(is_teacher)
+
+# Boolean in if statement
+age = 17
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
